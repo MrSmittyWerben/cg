80bc2582835a664d80f6def1b193b7105c6b3da1
Christian Zekovic <chris.512@hotmail.de>
2020-05-25 11:45:50 +0200

    aufg3 works, but lines are black for some reason

--- Blatt5/bresenham.py
+++ Blatt5/bresenham.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 import sys
 
@@ -39,28 +40,60 @@ def drawLines():
 
 def drawBresenhamLine(p, q):
     """ draw a line using bresenhams algorithm """
+    coords = []
+    swapped = False
+
     x0 = int(p[0])
     y0 = int(p[1])
     x1 = int(q[0])
     y1 = int(q[1])
 
-    a,b = y1-y0, x0-x1
+    a, b = y1-y0, x0-x1
+
+    m = math.fabs(a)/math.fabs(b) if b != 0 else 0  # element von [0,1] sonst spiegeln/rotieren
+
+    if m>1:
+        x0, y0 = y0, x0
+        x1, y1 = y1, x1
+
+    if x1 <= x0:
+        x1, x0 = x0, x1
+        y1, y0 = y0, y1
+        swapped = True
+
+
+    a, b = y1 - y0, x0 - x1  # erneut ausrechnen mit neuen Werten
+
+    a = -a if y1-y0 < 0 else a
+    b = -b if x1-x0 < 0 else b
+
     d = 2*a + b
     incE = 2*a
     incNE = 2*(a+b)
     y = y0
     for x in range(x0,x1+1):
-        element = can.create_rectangle(x - HPSIZE, y - HPSIZE,
-                                       x + HPSIZE, y + HPSIZE,
-                                       fill=FCOLOR, outline=BCOLOR)
-        elementList.append(element)
-        if d<= 0:
+        coords.append((x,y)) if 0 < m < 1 else coords.append((y,x))
+        if d <= 0:
             d += incE
         else:
             d += incNE
-            y += 1
-    pass
+            y += 1 if y0 < y1 else -1
 
+    if swapped:
+        coords.reverse()
+
+    print(coords)
+
+    for x,y in coords:
+        element = can.create_rectangle(x - 0.5*HPSIZE, y - 0.5*HPSIZE,
+                                       x + 0.5*HPSIZE, y + 0.5*HPSIZE,
+                                       fill=FCOLOR, outline=BCOLOR)
+        elementList.append(element)
+
+    if swapped:
+        coords.reverse
+
+    pass
 
 def quit(root=None):
     """ quit programm """
