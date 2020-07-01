6679b5861985c6fb87d56062df5233032448d175
Christian Zekovic <chris.512@hotmail.de>
2020-07-01 21:33:20 +0200

    drawing points

--- BSplines/RenderWin.py
+++ BSplines/RenderWin.py
@@ -2,12 +2,8 @@ import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-
 import numpy as np
-
-from oglRenderer.Scene import Scene
-
-global objFile, vbo
+from BSplines.SplineScene import SplineScene
 
 
 class RenderWin:
@@ -31,7 +27,7 @@ class RenderWin:
         # make a window
         self.width, self.height = 640, 480
         self.aspect = self.width / float(self.height)
-        self.window = glfw.create_window(self.width, self.height, "2D Graphics", None, None)
+        self.window = glfw.create_window(self.width, self.height, "B-Splines", None, None)
         if not self.window:
             glfw.terminate()
             return
@@ -43,7 +39,6 @@ class RenderWin:
         glfw.set_mouse_button_callback(self.window, self.onMouseButton)
         glfw.set_cursor_pos_callback(self.window, self.mouseMoved)
         glfw.set_key_callback(self.window, self.onKeyboard)
-        glfw.set_window_size_callback(self.window, self.onSize)
         glfw.set_scroll_callback(self.window, self.onScroll)
 
         # cursor position
@@ -53,47 +48,26 @@ class RenderWin:
         self.lastPosX = 0
         self.lastPosY = 0
 
-        # create 3D
-        self.scene = Scene(objFile, self.width, self.height)
+        # create Scene
+        glViewport(0, 0, self.width, self.height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, self.width, 0.0, self.height, 1.0, -1.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        self.scene = SplineScene(self.width, self.height)
 
         # exit flag
         self.exitNow = False
 
     def onMouseButton(self, win, button, action, mods):
         print("mouse button: ", win, button, action, mods)
-        r = min(self.width, self.height) / 2.0
-        ## Rotation
         if button == glfw.MOUSE_BUTTON_LEFT:
             if action == glfw.PRESS:
                 x, y = glfw.get_cursor_pos(win)
-                self.scene.doRotation = True
-                self.scene.startP = self.scene.projectOnSpehre(x, y, r)
-            elif action == glfw.RELEASE:
-                self.scene.doRotation = False
-                self.scene.actOri = np.dot(self.scene.actOri, self.scene.rotate(self.scene.angle, self.scene.axis))
-                self.scene.angle = 0
-
-        if button == glfw.MOUSE_BUTTON_RIGHT:
-            if action == glfw.PRESS:
-                self.scene.doMove = True
-            elif action == glfw.RELEASE:
-                self.scene.doMove = False
-
-        ## Zoom
-        if button == glfw.MOUSE_BUTTON_MIDDLE:
-            self.scene.zoomFactor += 0.1
+                self.scene.setPoint(x, y)
 
     def mouseMoved(self, window, x, y):
-        if self.scene.doRotation:
-            r = min(self.width, self.height) / 2.0
-            self.scene.moveP = self.scene.projectOnSpehre(x, y, r)
-            dot = np.dot(self.scene.startP, self.scene.moveP)
-            if dot < -1:  # arccos values must be between -1 and 1
-                dot = -1
-            if dot > 1:
-                dot = 1
-            self.scene.angle = np.arccos(dot)
-            self.scene.axis = np.cross(self.scene.startP, self.scene.moveP)
 
         if self.scene.doMove:
             offX = x - self.lastPosX
@@ -124,18 +98,7 @@ class RenderWin:
             if key == glfw.KEY_ESCAPE:
                 self.exitNow = True
 
-            ## orthogonal <-> perspective
-            if key == glfw.KEY_O:
-                self.scene.perspective = False
-                glfw.set_window_title(win, '2D Scene - Orthographic')
-
-            if key == glfw.KEY_P:
-                self.scene.perspective = True
-                glfw.set_window_title(win, '2D Scene - Perspective')
-
-
             ## COLORS
-
             if mods == glfw.MOD_SHIFT:  # shift pressed (background color)
                 if key == glfw.KEY_S:  # black
                     self.scene.actBgColor = self.scene.colors['BLACK']
@@ -159,46 +122,6 @@ class RenderWin:
                     self.scene.actColor = self.scene.colors['BLUE']
                 if key == glfw.KEY_G:  # yellow
                     self.scene.actColor = self.scene.colors['YELLOW']
-
-                if key == glfw.KEY_H:  # shadows
-                    self.scene.hasShadow = not self.scene.hasShadow
-
-                if key == glfw.KEY_N:  # shadows
-                    self.scene.wireMode = not self.scene.wireMode
-
-    def onSize(self, win, width, height):  # reshape
-        print("onsize: ", win, width, height)
-        # prevent division by zero
-        if height == 0:
-            height = 1
-
-        if width == 0:
-            width = 1
-
-        self.width = width
-        self.height = height
-        self.aspect = width / float(height)
-        glViewport(0, 0, self.width, self.height)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        if self.scene.perspective:
-            if width <= height:
-                gluPerspective(45*(height/float(width)), self.aspect, 0.1, 100)
-            else:
-                gluPerspective(45 * self.aspect, self.aspect, 0.1, 100)
-            gluLookAt(0, 0, 4, 0, 0, 0, 0, 1, 0)
-
-        else:
-            if width <= height:
-                glOrtho(-1.5, 1.5,
-                        -1.5 * height / width, 1.5 * height / width,
-                        -5.0, 5.0)
-            else:
-                glOrtho(-1.5 * width / height, 1.5 * width / height,
-                        -1.5, 1.5,
-                        -5.0, 5.0)
-
-        glMatrixMode(GL_MODELVIEW)
 
     def run(self):
 
@@ -224,14 +147,4 @@ def main():
 
 # call main
 if __name__ == '__main__':
-
-    if len(sys.argv) == 2:
-        if sys.argv[1].lower() in ('bunny', 'cow', 'squirrel', 'squirrel2', 'elephant'):
-            objFile = f"{sys.argv[1].lower()}.obj"
-        else:
-            print('Invalid arguments received!')
-            sys.exit(-1)
-    else:
-        sys.exit(-1)
-
     main()
---
+++ BSplines/SplineScene.py
@@ -0,0 +1,73 @@
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.arrays import vbo
+
+import numpy as np
+
+
+class SplineScene:
+    """ OpenGL 2D scene class """
+
+    # initialization
+    def __init__(self, width, height):
+        self.pointsize = 5.0
+        self.linewidth = 3.0
+        self.width = width
+        self.height = height
+        glPointSize(self.pointsize)
+        glLineWidth(self.linewidth)
+
+        # movement
+        self.doMove = False
+
+        # Colors
+        self.red = 1.0, 0.0, 0.0, 1.0
+        self.green = 0.0, 1.0, 0.0, 1.0
+        self.blue = 0.0, 0.0, 1.0, 1.0
+        self.yellow = 1.0, 1.0, 0.0, 1.0
+        self.white = 1.0, 1.0, 1.0, 1.0
+        self.black = 0.0, 0.0, 0.0, 1.0
+
+        self.colors = {
+            'RED': self.red,
+            'GREEN': self.green,
+            'BLUE': self.blue,
+            'YELLOW': self.yellow,
+            'WHITE': self.white,
+            'BLACK': self.black
+        }
+
+        self.bgColors = {
+            'RED': self.red,
+            'GREEN': self.green,
+            'BLUE': self.blue,
+            'YELLOW': self.yellow,
+            'WHITE': self.white,
+            'BLACK': self.black
+        }
+
+        self.actColor = 0.0, 0.0, 0.0, 0.0
+        self.actBgColor = 1.0, 1.0, 1.0, 1.0
+
+        self.controlPoints = []
+
+    # render
+    def render(self, width, height):
+
+        glClearColor(*self.actBgColor)
+        glColor(self.actColor)
+
+        data = vbo.VBO(np.array(self.controlPoints, 'f'))
+        data.bind()
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glVertexPointer(2, GL_FLOAT, 0, data)
+        glDrawArrays(GL_POINTS, 0, len(self.controlPoints))
+        data.unbind()
+        glDisableClientState(GL_VERTEX_ARRAY)
+        glFlush()
+
+    def setPoint(self, x, y):
+        self.controlPoints.extend((x, self.height-y))
+        print(self.controlPoints)
